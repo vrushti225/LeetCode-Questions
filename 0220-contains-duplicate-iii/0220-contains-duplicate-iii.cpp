84979e7e79a41d2ffa6f14dc4c266568021c5fa9
vrushti225 <vrushtibhut811@gmail.com>
2026-06-29 10:29:48 +0530

    Time: 176 ms (40.36%), Space: 157.1 MB (48.96%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff)
+    {
+        set<long long>window;
+        for (int i = 0; i < nums.size(); i++)
+        {
+        auto it = window.lower_bound((long long)nums[i] - valueDiff);
+        if (it != window.end() && *it <= (long long)nums[i] + valueDiff)
+                return true;
+                window.insert(nums[i]);
+                if (i >= indexDiff)
+                window.erase(nums[i - indexDiff]);
+        }
+
+        return false;
+    }
+};
