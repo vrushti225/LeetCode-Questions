309e5f1ff433e68900f82f9368e980ca6d06cdcc
vrushti225 <vrushtibhut811@gmail.com>
2026-06-29 19:53:00 +0530

    Time: 2 ms (100%), Space: 46.5 MB (62.59%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int romanToInt(String s) {
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = value(s.charAt(i));
+
+            if (i < s.length() - 1 && current < value(s.charAt(i + 1))) {
+                sum -= current;
+            } else {
+                sum += current;
+            }
+        }
+
+        return sum;
+    }
+
+    public int value(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+}
